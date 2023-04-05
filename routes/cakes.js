0bbfebf34d3cbe4fd7596e18361c2cf288c903cf
sirishapanu03 <sirisha.panu03@gmail.com>
2023-04-05 18:56:23 -0500

    updated cakes

--- routes/cakes.js
+++ routes/cakes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('cakes', { title: 'Cakes' });
+  res.render('cakes', { title: 'Search results' });
 });
 
 module.exports = router;
