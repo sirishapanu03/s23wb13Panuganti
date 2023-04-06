ccc08507d1a654563685614dba9547764376db81
sirishapanu03 <sirisha.panu03@gmail.com>
2023-04-05 19:17:08 -0500

    added board

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cakesRouter = require('./routes/cakes');
+var boardRouter = require('./routes/board');
+
 
 
 var app = express();
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cakes', cakesRouter);
+app.use('/board', boardRouter);
 
 
 // catch 404 and forward to error handler
