3a522abfb10294b74ddb3b01662334b394c2aa9b
sirishapanu03 <sirisha.panu03@gmail.com>
2023-04-05 20:06:30 -0500

    added selector
    added selector

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cakesRouter = require('./routes/cakes');
 var boardRouter = require('./routes/board');
-
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -27,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cakes', cakesRouter);
 app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
 
 // catch 404 and forward to error handler
